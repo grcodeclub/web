38ebedf4db2cffda163947c5908b81e2d252308e
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-22 19:07:42 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/website/main.html">Ιστοσελίδες</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Γλώσσες Προγραμματισμού</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/html-css.html">CSS - Html</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/php.html">Php</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/javascript.html">JavaScript</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
