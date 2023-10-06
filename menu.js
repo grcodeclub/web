9f3413c9c13da1355c6d0f4b53dc39640f003c8b
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-10-06 16:02:14 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -9,19 +9,18 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.github.io/programming">Γλώσσες Προγραμματισμού</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = ' <li><a href="https://grcodeclub.github.io/website/html-css.html">CSS - Html</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/post/html-css.html">CSS - Html</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/website/php.html">Php</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/post/php.html">Php</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
     menuLink = ' <li><a href="https://grcodeclub.github.io/javascript">JavaScript</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/website/extra/symbols.html">Symbols</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/website/post/more/symbols.html">Symbols</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
   });
-  
