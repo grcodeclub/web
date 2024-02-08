be62e70d8fc886bc9de073e38a5de79ac058d14b
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-08 18:33:34 +0200

    Update and rename menu.js to menu.js

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    
+    var  menuLink = '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.gr/website">Ιστοσελίδες</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.gr/website/html-css">CSS - Html</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/website/php">Php</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/javascript">JavaScript</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/website/more/">Περισσότερα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/programming">Γλώσσες Προγραμματισμού</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
