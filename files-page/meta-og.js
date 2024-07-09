c62649caeb75d79b277ce35813443f88a7fe7055
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-07-09 14:04:36 +0300

    Delete files-page directory

--- files-page/meta-og.js
+++
@@ -1,8 +0,0 @@
-    var metaTag = document.createElement('meta');
-
-    // Ορισμός των ιδιοτήτων του meta tag
-    metaTag.setAttribute('property', 'og:image');
-    metaTag.setAttribute('content', 'https://grcodeclub.github.io/files-page/background/website.jpg');
-
-    // Προσθήκη του meta tag στο <head> της σελίδας
-    document.head.appendChild(metaTag);
