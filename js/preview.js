98d76a645fcb5454b1644a0fab44edaec3477dfb
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-07-09 14:02:14 +0300

    Delete js/preview.js

--- js/preview.js
+++
@@ -1,9 +0,0 @@
-
-    var metaTag = document.createElement('meta');
-
-    // Ορισμός των ιδιοτήτων του meta tag
-    metaTag.setAttribute('property', 'og:image');
-    metaTag.setAttribute('content', 'https://grcodeclub.github.io/files-page/preview/website.png');
-
-    // Προσθήκη του meta tag στο <head> της σελίδας
-    document.head.appendChild(metaTag);
