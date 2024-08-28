0e06b2fadc82855ddb09fec78c171bc87b305ce4
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-28 21:54:56 +0300

    Add files via upload

---
+++ docs/css/grcodeclub/scrollbars.css
@@ -0,0 +1,18 @@
+/* Custom Scrollbars   */
+    
+::-webkit-scrollbar {width: 10px;} /* width */
+
+/* Track */
+::-webkit-scrollbar-track {
+      box-shadow: inset 0 0 10px rgb(4, 4, 48); 
+      border-radius: 10px; }
+     
+/* Handle */
+::-webkit-scrollbar-thumb {
+      background: rgb(253, 253, 253); 
+      border-radius: 5px;
+    }
+ 
+::-webkit-scrollbar-thumb:hover {background: rgb(12, 12, 1);} 
+  
+/* ΤΕΛΟΣ Custom Scrollbars   */
---
+++ docs/js/menu.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.gr/web">Ιστοσελίδες</a></li>',
+        '<li><a href="https://grcodeclub.gr/web/css">CSS</a></li>',
+        '<li><a href="https://grcodeclub.gr/web/html">Html</a></li>',
+        '<li><a href="https://grcodeclub.gr/php">Php</a></li>',
+        '<li><a href="https://grcodeclub.gr/javascript">JavaScript</a></li>',
+        '<li><a href="https://grcodeclub.gr/nodejs">Node.js</a></li>',
+        '<li><a href="https://grcodeclub.gr/web/orologia">Oρολογία</a></li>',
+        '<li><a href="https://grcodeclub.gr/web/cms/wordpress">WordPress</a></li>',
+        '<li><a href="https://grcodeclub.gr/web/more/">Περισσότερα</a></li>',
+        '<li><a href="https://grcodeclub.gr/programming/">Γλώσσες Προγραμματισμού</a></li>',
+        '<li style="height: 200px;"></li>'
+    ];
+    
+    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+    menuLinks.forEach(function(link) {
+        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+    });
+});
+};
