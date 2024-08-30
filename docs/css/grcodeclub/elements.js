e5d3887363ed96cc9a0014b0342e5a91f121c729
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-30 14:06:49 +0300

    Create elements.js

---
+++ docs/css/grcodeclub/elements.js
@@ -0,0 +1,12 @@
+            .arrow {
+    display: inline-block;
+    transition: transform 0.3s ease; /* Προσθέτει μια ομαλή μετάβαση */
+}
+
+.arrow.down {
+    transform: rotate(-90deg); /* Δείχνει προς τα κάτω */
+}
+
+.arrow.up {
+    transform: rotate(-90deg); /* Δείχνει προς τα πάνω */
+}
